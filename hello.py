4c9f9747762e24f85e6b22a1b5ff3eabd88c2ffd
iiot-tbb <247687157@qq.com>
2019-10-26 21:30:48 +0800

    python study

---
+++ hello.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+#hi
+#how are you?
+#what is this 
+#this is python
+
+a =100;
+if a>=0:
+    print(a)
+else:
+    print(-a)
+
+name = 'tbb'
+
+print('hello',name)
+
+print('hello,world')
+print("hello woirld")
+print("hell'")
+print('I\'m ok\\\n')
+print('I\\\t\\')
+print(r'\\\t\\')
+print('''line
+line2
+line3''')
+print(r'''hello,\n
+world''')
+age =20
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+a = 'acb#a' #a变为字符号串
+print(a)
+b =a
+a ='xyz'
+print(b)
+
+n=123
+f=456.789
+s1 = 'hello,world'
+s2 = 'Hello,\'Adam\''
+s3 = r'Hello,"Bert'
+s4 = r'''Hello,
+Lisa!'''
+print('包含中文的str')
+x=b'ABC'
+print('ABC')
+
---
+++ print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding=utf-8
+print(300);
+print('1+2 = ', 1+2)
+print('the quick brown fox'  'jumps over'  'the lazy dog')
