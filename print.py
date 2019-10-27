6f893cee5c59e381439e2dd2eab03a506c7929f6
iiot-tbb <247687157@qq.com>
2019-10-27 11:14:36 +0800

    of files

--- print.py
+++ print.py
@@ -6,4 +6,4 @@ print('the quick brown fox'  'jumps over'  'the lazy dog')
 #git is used by many programmer
 #git is free software distrubed under the GPL
 #git has a mutable index called stage.
-#git tracks changes 
+#git tracks changes of files 
