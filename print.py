6a5510de854df1c75a03c98357237c7009fcf90e
iiot-tbb <247687157@qq.com>
2019-10-27 10:46:16 +0800

    understand how stage work

--- print.py
+++ print.py
@@ -5,3 +5,5 @@ print('1+2 = ', 1+2)
 print('the quick brown fox'  'jumps over'  'the lazy dog')
 #git is used by many programmer
 #git is free software distrubed under the GPL
+#git has a mutable index called stage.
+
